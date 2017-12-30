6e45ca5ef7faf9ae4a80774017f67433d5e6275c
Jakub Mandra <mandra.jakub@gmail.com>
2017-12-30 19:58:33 +0100

    Initial commit

---
+++ 01-DrumKit/scripts.js
@@ -0,0 +1,33 @@
+function playOnKey(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return; // stop function from running all keys
+
+  audio.currentTime = 0; // rewind audio to start
+  audio.play();
+
+  key.classList.add('playing');
+}
+
+function playOnPointer(e) {
+  const keyCode = this.dataset.key;
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+  audio.currentTime = 0; // rewind audio to start
+  audio.play();
+
+  this.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip it if its not transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(function(key) {
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('mouseenter', playOnPointer);
+});
+
+window.addEventListener('keydown', playOnKey);
